22f8a88666e1bbfce45c66c0317f37ee0655b910
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-16 14:16:49 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+ let num1 = 2;
+ let num2 = 31;
+ let multiply = num1*num2;
 
-// Write your code here
+
+let random = Math.floor(Math.random()*10) + 1;
+
+
+let num3 = 40;
+let num4 = 6;
+let mod = num3%num4;
+
+let max = Math.max(5,10,15,20);
+
+
+function mod(num3,num4) {
+num3=40;
+num4=6;
+mod = num3%num4;
+} 
+mod()  
+
+  function max(){
+    Math.max(5,10,15,20);
+    max;
+  }  
